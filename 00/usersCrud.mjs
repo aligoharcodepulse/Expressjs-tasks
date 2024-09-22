12fc1b6faae19ade8f441a94f3733ccbab2720b0
Ali Gohar <aligoharkp@gmail.com>
2024-09-22 17:50:06 +0500

    done with post request

--- 00/usersCrud.mjs
+++ 00/usersCrud.mjs
@@ -2,7 +2,7 @@ import express from 'express'
 
 const app = express()
 app.use(express.json())
-
+const PORT = 3000;
 const users = [
     {id:1, name:"Ali", email:"ali@gmail.com"},
     {id:2, name:"Alam", email:"alam@gmail.com"}
@@ -11,7 +11,19 @@ const users = [
 app.get('/api/users',(req,res)=>{
     res.status(200).json({message:'GET Request - Fetching all items!', users})
 })
-const PORT = 3000;
+
+//POST Request
+app.post('/api/users',(req,res)=>{
+    const body = req.body;
+    const newUser = {
+        id: users.length + 1,
+        ...body
+    }
+    users.push(newUser)
+    res.status(201).json({message:'POST Request - Adding new item!', newUser})
+        
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Server is listening at http://localhost:${PORT}`);
