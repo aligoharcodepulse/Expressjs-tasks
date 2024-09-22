301a5cb22a850668cef88f087283ef9832cf2454
Ali Gohar <aligoharkp@gmail.com>
2024-09-22 17:38:41 +0500

    done with get request

--- 00/usersCrud.mjs
+++ 00/usersCrud.mjs
@@ -1,9 +1,15 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.end('GET Request - Fetching all items!')
+const users = [
+    {id:1, name:"Ali", email:"ali@gmail.com"},
+    {id:2, name:"Alam", email:"alam@gmail.com"}
+]
+//GET Request
+app.get('/api/users',(req,res)=>{
+    res.status(200).json({message:'GET Request - Fetching all items!', users})
 })
 const PORT = 3000;
 
