bf21c44943766865f4b4d3680bb5193804b5d754
Ali Gohar <aligoharkp@gmail.com>
2024-09-29 11:46:30 +0500

    done with get request and error handler function

--- 01-userManagement/backend/server.mjs
+++ 01-userManagement/backend/server.mjs
@@ -6,11 +6,25 @@ app.use(cors())
 app.use(express.json())
 const PORT = 3000;
 
+const users = [
+    {id:1, name:'Ali', email:'ali@gmail.com'},
+    {id:2, name:'Alam', email:'alam@gmail.com'}
+]
+
+function errorHandler(err, req, res, next) {
+    res.status(err.status || 500).json({
+        message: err.message || 'Something went wrong',
+        error: true
+    })
+}
+
+//Get all users
 app.get('/',(req,res)=>{
-    res.status(200).json({message:'Get All Users'})
+    res.status(200).json({message:'Fetching All Users',data: users})
 })
 
 
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`Server is listening at: http://localhost:${PORT}`);
