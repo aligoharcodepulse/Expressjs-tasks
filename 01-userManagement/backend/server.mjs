c47b8c39d4286bbc15b19cb6e4936c5aa51f47d0
Ali Gohar <aligoharkp@gmail.com>
2024-09-29 11:43:04 +0500

    backend server created

--- 01-userManagement/backend/server.mjs
+++ 01-userManagement/backend/server.mjs
@@ -1,4 +1,17 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message:'Get All Users'})
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening at: http://localhost:${PORT}`);
+})
